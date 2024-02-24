a5b5d44bebdc2c621dfae873a75cd9f8bf8c69af
Kwayu Mmari <mmaridenis@gmail.com>
2024-02-24 09:10:45 +0300

    fixing get all roles

--- src/app/roles/page.tsx
+++ src/app/roles/page.tsx
@@ -52,44 +52,35 @@ export default function Page() {
         {error}
       </Alert>
       <div className="row">
-        <div className="col-sm-6 col-lg-3">
-          <Card bg="primary" text="white" className="mb-4">
-            <CardBody className="pb-0 d-flex justify-content-between align-items-start">
-              <div>
-                <div className="fs-4 fw-semibold">
-                  26K
-                  <span className="fs-6 ms-2 fw-normal">
-                    (-12.4%
-                    <FontAwesomeIcon icon={faArrowDown} fixedWidth />
-                    )
-                  </span>
+        {roles.map(role => (
+          <div className="col-sm-6 col-lg-3">
+            <Card bg="primary" text="white" className="mb-4">
+              <CardBody className="pb-0 d-flex justify-content-between align-items-start">
+                <div>
+                  <div>{role.name}</div>
                 </div>
-                <div>Users</div>
+                <Dropdown align="end">
+                  <DropdownToggle
+                    as="button"
+                    bsPrefix="btn"
+                    className="btn-link rounded-0 text-white shadow-none p-0"
+                    id="dropdown-chart1"
+                  >
+                    <FontAwesomeIcon fixedWidth icon={faEllipsisVertical} />
+                  </DropdownToggle>
+
+                  <DropdownMenu>
+                    <DropdownItem href="#/action-1">Edit Role</DropdownItem>
+                    <DropdownItem href="#/action-2">Another action</DropdownItem>
+                    <DropdownItem href="#/action-3">Something else</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </CardBody>
+              <div className="mt-3 mx-3" style={{ height: '70px' }}>
               </div>
-              <Dropdown align="end">
-                <DropdownToggle
-                  as="button"
-                  bsPrefix="btn"
-                  className="btn-link rounded-0 text-white shadow-none p-0"
-                  id="dropdown-chart1"
-                >
-                  <FontAwesomeIcon fixedWidth icon={faEllipsisVertical} />
-                </DropdownToggle>
-
-                <DropdownMenu>
-                  <DropdownItem href="#/action-1">Action</DropdownItem>
-                  <DropdownItem href="#/action-2">Another action</DropdownItem>
-                  <DropdownItem href="#/action-3">Something else</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </CardBody>
-            <div className="mt-3 mx-3" style={{ height: '70px' }}>
-              <UserChart />
-            </div>
-          </Card>
-        </div>
-
-        
+            </Card>
+          </div>
+        ))}
       </div>
     </>
   )
