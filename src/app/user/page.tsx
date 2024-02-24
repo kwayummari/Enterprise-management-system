62e057c3c659e8ee5d682f61c1b050f822b82a5a
Kwayu Mmari <mmaridenis@gmail.com>
2024-02-24 07:46:27 +0300

    fixing get all users

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -81,8 +81,9 @@ export default function Page() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr className="align-middle">
-                      {/* <td className="text-center">
+                    {users.map(user => (
+                      <tr className="align-middle">
+                        {/* <td className="text-center">
                         <div className="avatar avatar-md d-inline-flex position-relative">
                           <Image
                             fill
@@ -96,59 +97,59 @@ export default function Page() {
                           />
                         </div>
                       </td> */}
-                      <td>
-                        <div>Yiorgos Avraamu</div>
-                        <div className="small text-black-50">
-                          <span>New</span>
-                          {' '}
-                          | Registered: Jan 1, 2020
-                        </div>
-                      </td>
-                      <td>
-                        <div className="clearfix">
-                          <div className="float-start">
-                            <div className="fw-semibold">50%</div>
+                        <td>
+                          <div>{user.fullname}</div>
+                          <div className="small text-black-50">
+                            <span>New</span>
+                            {' '}
+                            | Registered: Jan 1, 2020
                           </div>
-                          <div className="float-end">
-                            <small className="text-black-50">
-                              Jun 11, 2020 - Jul 10, 2020
-                            </small>
+                        </td>
+                        <td>
+                          <div className="clearfix">
+                            <div className="float-start">
+                              <div className="fw-semibold">50%</div>
+                            </div>
+                            <div className="float-end">
+                              <small className="text-black-50">
+                                Jun 11, 2020 - Jul 10, 2020
+                              </small>
+                            </div>
                           </div>
-                        </div>
-                        <ProgressBar className="progress-thin" variant="success" now={50} />
-                      </td>
-                      <td className="text-center" aria-label="icon">
-                        <FontAwesomeIcon icon={faCcAmex} size="lg" fixedWidth />
-                      </td>
-                      <td>
-                        <div className="small text-black-50">Last login</div>
-                        <div className="fw-semibold">10 sec ago</div>
-                      </td>
-                      <td>
-                        <Dropdown align="end">
-                          <DropdownToggle
-                            as="button"
-                            bsPrefix="btn"
-                            className="btn-link rounded-0 text-black-50 shadow-none p-0"
-                            id="action-user1"
-                          >
-                            <FontAwesomeIcon fixedWidth icon={faEllipsisVertical} />
-                          </DropdownToggle>
-
-                          <DropdownMenu>
-                            <DropdownItem href="#/action-1">Info</DropdownItem>
-                            <DropdownItem href="#/action-2">Edit</DropdownItem>
-                            <DropdownItem
-                              className="text-danger"
-                              href="#/action-3"
+                          <ProgressBar className="progress-thin" variant="success" now={50} />
+                        </td>
+                        <td className="text-center" aria-label="icon">
+                          <FontAwesomeIcon icon={faCcAmex} size="lg" fixedWidth />
+                        </td>
+                        <td>
+                          <div className="small text-black-50">Last login</div>
+                          <div className="fw-semibold">10 sec ago</div>
+                        </td>
+                        <td>
+                          <Dropdown align="end">
+                            <DropdownToggle
+                              as="button"
+                              bsPrefix="btn"
+                              className="btn-link rounded-0 text-black-50 shadow-none p-0"
+                              id="action-user1"
                             >
-                              Delete
-                            </DropdownItem>
-                          </DropdownMenu>
-                        </Dropdown>
-                      </td>
-                    </tr>
-                    
+                              <FontAwesomeIcon fixedWidth icon={faEllipsisVertical} />
+                            </DropdownToggle>
+
+                            <DropdownMenu>
+                              <DropdownItem href="#/action-1">Info</DropdownItem>
+                              <DropdownItem href="#/action-2">Edit</DropdownItem>
+                              <DropdownItem
+                                className="text-danger"
+                                href="#/action-3"
+                              >
+                                Delete
+                              </DropdownItem>
+                            </DropdownMenu>
+                          </Dropdown>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
