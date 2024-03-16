6477a2eba5f928ebc9b54fd8d2950e22d6f88837
Kwayu Mmari <mmaridenis@gmail.com>
2024-03-16 03:24:58 +0300

    fixing permissions

--- src/app/ui/dashboard/Sidebar/SidebarNav.tsx
+++ src/app/ui/dashboard/Sidebar/SidebarNav.tsx
@@ -1,29 +1,29 @@
-'use client'
-import {
-   faFileLines
-} from '@fortawesome/free-regular-svg-icons'
+"use client";
+import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 import {
   faCodeBranch,
   faFolder,
   faGear,
   faLayerGroup,
   faUserGear,
-} from '@fortawesome/free-solid-svg-icons'
-import React, { PropsWithChildren, useState, useEffect } from 'react'
-import { Alert, Badge } from 'react-bootstrap'
-import SidebarNavGroup from '@/app/ui/dashboard/Sidebar/SidebarNavGroup'
-import SidebarNavItem from '@/app/ui/dashboard/Sidebar/SidebarNavItem'
-import apiGateway from '@/app/gateway/gateways'
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
-import { useRouter } from 'next/navigation'
-import { faSlack } from '@fortawesome/free-brands-svg-icons'
+} from "@fortawesome/free-solid-svg-icons";
+import React, { PropsWithChildren, useState, useEffect } from "react";
+import { Alert, Badge } from "react-bootstrap";
+import SidebarNavGroup from "@/app/ui/dashboard/Sidebar/SidebarNavGroup";
+import SidebarNavItem from "@/app/ui/dashboard/Sidebar/SidebarNavItem";
+import apiGateway from "@/app/gateway/gateways";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { useRouter } from "next/navigation";
+import { faSlack } from "@fortawesome/free-brands-svg-icons";
 
 const SidebarNavTitle = (props: PropsWithChildren) => {
-  const { children } = props
+  const { children } = props;
   return (
-    <li className="nav-title px-3 py-2 mt-3 text-uppercase fw-bold">{children}</li>
-  )
-}
+    <li className="nav-title px-3 py-2 mt-3 text-uppercase fw-bold">
+      {children}
+    </li>
+  );
+};
 interface Permissions {
   id: number;
   name: string;
@@ -32,13 +32,13 @@ interface Permissions {
   increase: string;
   upgrade: string;
   remove: string;
-  submenu: { name: string, url: string, crud: string }[]; 
+  submenu: { name: string; url: string; crud: string }[];
 }
 
 export default function SidebarNav() {
   const router = useRouter();
-  const [error, setError] = useState<string | null>('');
-  const roleId = localStorage.getItem('roleId');
+  const [error, setError] = useState<string | null>("");
+  const roleId = localStorage.getItem("roleId");
   const [permissions, setPermissions] = useState<Permissions[]>([]);
 
   useEffect(() => {
@@ -50,7 +50,7 @@ export default function SidebarNav() {
       const userData = {
         id: roleId,
       };
-      const value = await apiGateway.create('getPermission', userData);
+      const value = await apiGateway.create("getPermission", userData);
       setPermissions(value.contents);
     } catch (err: any) {
       setError(err.message);
@@ -60,47 +60,72 @@ export default function SidebarNav() {
     <>
       <Alert
         variant="danger"
-        show={error !== ''}
-        onClose={() => setError('')}
+        show={error !== ""}
+        onClose={() => setError("")}
         dismissible
       >
         {error}
       </Alert>
-       <ul className="list-unstyled">
-      <SidebarNavItem icon={faSlack} href="/dashboard">
-        Dashboard
-        <small className="ms-auto"><Badge bg="info" className="ms-auto">NEW</Badge></small>
+      <ul className="list-unstyled">
+        <SidebarNavItem icon={faSlack} href="/dashboard">
+          Dashboard
+          <small className="ms-auto">
+            <Badge bg="info" className="ms-auto">
+              NEW
+            </Badge>
+          </small>
         </SidebarNavItem>
         {/* <SidebarNavTitle>Components</SidebarNavTitle> */}
-        {permissions.map(permission => (
-          (permission.increase === '1' && permission.find === '1' && permission.upgrade === '1' && permission.remove === '1') &&
-          <SidebarNavGroup key={permission.id} toggleIcon={faFolder} toggleText={permission.name}>
-            {permission.submenu.map(submenu => (
-              (submenu.crud === '1' && permission.increase === '1') ?
-                <SidebarNavItem href={submenu.url}>{submenu.name}</SidebarNavItem>
-                :
-                (submenu.crud === '2' && permission.find === '1') ?
-                  <SidebarNavItem href={submenu.url}>{submenu.name}</SidebarNavItem> 
-                  :
-                  (submenu.crud === '3' && permission.upgrade === '1') ?
-                    <SidebarNavItem href={submenu.url}>{submenu.name}</SidebarNavItem> 
-                    :
-                    (submenu.crud === '4' && permission.remove === '1') ?
-                      <SidebarNavItem href={submenu.url}>{submenu.name}</SidebarNavItem> 
-                      : ''
-            ))}
-            </SidebarNavGroup>
-        ))}
-      <SidebarNavTitle>Extras</SidebarNavTitle>
+        {permissions.map(
+          (permission) =>
+            permission.find === "1" && (
+              <SidebarNavGroup
+                key={permission.id}
+                toggleIcon={faFolder}
+                toggleText={permission.name}
+              >
+                {permission.submenu.map((submenu) =>
+                  submenu.crud === "1" && permission.increase === "1" ? (
+                    <SidebarNavItem href={submenu.url}>
+                      {submenu.name}
+                    </SidebarNavItem>
+                  ) : submenu.crud === "2" && permission.find === "1" ? (
+                    <SidebarNavItem href={submenu.url}>
+                      {submenu.name}
+                    </SidebarNavItem>
+                  ) : submenu.crud === "3" && permission.upgrade === "1" ? (
+                    <SidebarNavItem href={submenu.url}>
+                      {submenu.name}
+                    </SidebarNavItem>
+                  ) : submenu.crud === "4" && permission.remove === "1" ? (
+                    <SidebarNavItem href={submenu.url}>
+                      {submenu.name}
+                    </SidebarNavItem>
+                  ) : (
+                    ""
+                  )
+                )}
+              </SidebarNavGroup>
+            )
+        )}
+        <SidebarNavTitle>Extras</SidebarNavTitle>
 
-      <SidebarNavGroup toggleIcon={faGear} toggleText="Configurations">
-          <SidebarNavItem icon={faUserGear} href="roles">Roles</SidebarNavItem>
-          <SidebarNavItem icon={faCodeBranch} href="branch">Branch</SidebarNavItem>
-      </SidebarNavGroup>
+        <SidebarNavGroup toggleIcon={faGear} toggleText="Configurations">
+          <SidebarNavItem icon={faUserGear} href="roles">
+            Roles
+          </SidebarNavItem>
+          <SidebarNavItem icon={faCodeBranch} href="branch">
+            Branch
+          </SidebarNavItem>
+        </SidebarNavGroup>
 
-      <SidebarNavItem icon={faFileLines} href="#">Docs</SidebarNavItem>
-      <SidebarNavItem icon={faLayerGroup} href="https://jamsolutions.co.tz">Contact JamSolutions</SidebarNavItem>
-    </ul>
+        <SidebarNavItem icon={faFileLines} href="#">
+          Docs
+        </SidebarNavItem>
+        <SidebarNavItem icon={faLayerGroup} href="https://jamsolutions.co.tz">
+          Contact JamSolutions
+        </SidebarNavItem>
+      </ul>
     </>
-  )
+  );
 }
